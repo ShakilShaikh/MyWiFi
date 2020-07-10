584af2a0811455a2daf4f520310d658cc59427c6
Shakil Ibne Shaikh <kingshakil55@gmail.com>
2020-07-10 00:20:24 -0230

    Add files via upload

---
+++ MyWiFi/Profiles.py
@@ -0,0 +1,17 @@
+# In the name of Allah
+# MyWiFi , by Shakil Ibne Shaikh
+
+
+class Profiles:
+    def __init__(self):
+        self.ssid = ''
+        self.id = 0
+        self.ntype = ''
+        self.auth = ''
+        self.encryption =''
+        self.bssid = ''
+        self.signal = ''
+        self.radio = ''
+        self.channel=''
+        self.BasicRate=''
+        self.OtherRate=''
